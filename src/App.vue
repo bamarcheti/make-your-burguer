0367beaedf4ad3bde23d4671c50150cbcac7e3c6
bamarcheti <bmarchetifiorin@gmail.com>
2025-08-06 10:45:24 -0400

    style: Atualizado estilização do App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,42 @@
 <template>
   <Navbar :logo="logo_src" :alt="app_name" />
-  <router-view/>
+  <router-view />
   <Footer />
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
+import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   components: {
     Navbar,
-    Footer
+    Footer,
   },
   data() {
     return {
       logo_src: "/img/logo.png",
-      app_name: "Make Your Burguer"
-    }
-  }
-}
+      app_name: "Make Your Burguer",
+    };
+  },
+};
 </script>
 
 <style>
-  * {
-    font-family: Helvetica;
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-  .main-container {
-    margin: 50px;
-    min-height: 250px;
-  }
-  h1 {
-    text-align: center;
-    font-size: 42px;
-    margin-bottom: 30px;
-    color: #222;
-  }
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
